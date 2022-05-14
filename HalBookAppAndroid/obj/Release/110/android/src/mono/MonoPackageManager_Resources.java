7af913732c4b1eee102902a72f6409573eedd95e
birdLove <hbradt@uci.edu>
2022-05-14 14:06:30 -0700

    SDFS

---
+++ HalBookAppAndroid/obj/Release/110/android/src/mono/MonoPackageManager_Resources.java
@@ -0,0 +1,31 @@
+package mono;
+public class MonoPackageManager_Resources {
+	public static String[] Assemblies = new String[]{
+		/* We need to ensure that "HalBookAppAndroid.dll" comes first in this list. */
+		"HalBookAppAndroid.dll",
+		"BouncyCastle.Crypto.dll",
+		"MailKit.dll",
+		"MimeKit.dll",
+		"System.Security.Cryptography.Pkcs.dll",
+		"Tesseract.Binding.Droid.dll",
+		"Tesseract.dll",
+		"Tesseract.Droid.dll",
+		"Xamarin.AndroidX.Activity.dll",
+		"Xamarin.AndroidX.AppCompat.dll",
+		"Xamarin.AndroidX.CoordinatorLayout.dll",
+		"Xamarin.AndroidX.Core.dll",
+		"Xamarin.AndroidX.CustomView.dll",
+		"Xamarin.AndroidX.DrawerLayout.dll",
+		"Xamarin.AndroidX.Fragment.dll",
+		"Xamarin.AndroidX.Lifecycle.Common.dll",
+		"Xamarin.AndroidX.Lifecycle.LiveData.Core.dll",
+		"Xamarin.AndroidX.Lifecycle.ViewModel.dll",
+		"Xamarin.AndroidX.Loader.dll",
+		"Xamarin.AndroidX.SavedState.dll",
+		"Xamarin.Essentials.dll",
+		"Xamarin.Google.Android.Material.dll",
+		"Xamarin.Google.Guava.ListenableFuture.dll",
+	};
+	public static String[] Dependencies = new String[]{
+	};
+}
